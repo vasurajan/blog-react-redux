73a00e825bc9346918fe7aaa65bc44e84beb43c7
vasurajan <63305945+vasurajan@users.noreply.github.com>
2020-10-13 17:58:45 +0530

    reducer updated

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -8,11 +8,19 @@ class PostList extends React.Component {
         this.props.fetchPosts();
     }
     render() {
+        console.log(this.props.posts);
         return (
-            <div>Post List</div>
+            <div>
+                <div>List</div>
+              Post
+            </div>
+
         )
     }
-
 }
 
-export default connect(null, { fetchPosts })(PostList);
+const mapStateToProps = (state) => {
+    return { posts: state.posts };
+}
+
+export default connect(mapStateToProps, { fetchPosts })(PostList);
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,6 @@
-
 import { combineReducers } from 'redux';
+import postsReducer from './postsReducer';
 
 export default combineReducers({
-    replaceMe: () => "Hi"
+    posts: postsReducer
 });
